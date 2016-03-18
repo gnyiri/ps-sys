1a3da4ab116ecf09f1503b0c91bdb4a0933733d5
Gergely Nyiri <gergely.nyiri@gmail.com>
2016-03-18 13:53:30 +0100

    Update FileStat class

--- ps_utils/file_stat.py
+++ ps_utils/file_stat.py
@@ -7,7 +7,9 @@ import os
 class FileStat:
     def __init__(self, directory):
         self.m_directory = directory
-        self.m_file_extensions = []
+        self.m_file_count = 0
+        self.m_file_extensions = {}
+
     def lookup(self):
         assert isinstance(self.m_directory, str)
 
@@ -18,4 +20,14 @@ class FileStat:
             for root, dir, files in os.walk(self.m_directory):
                 for file in files:
                     print file
+                    self.m_file_count += 1
+                    file_ext = os.path.basename(file)
+                    if self.m_file_extensions.has_key(file_ext):
+                        self.m_file_extensions[file_ext] += 1
+                    else:
+                        self.m_file_extensions[file_ext] = 0
 
+            print self.m_file_count
+
+            for key in self.m_file_extensions:
+                print key, ' => ', self.m_file_extensions[key]
--- py-sys.py
+++ py-sys.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import sys
-import ps_utils
+from ps_utils.file_stat import FileStat
 
 
 def main():
     fs = FileStat(sys.argv[1])
+    fs.lookup()
 
 if __name__ == '__main__':
     main()
